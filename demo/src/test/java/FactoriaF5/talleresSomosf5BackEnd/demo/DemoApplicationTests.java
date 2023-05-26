fa6d9517fe92e95a7c003300346271852f3692ba
ifadoua2 <f647379@gmail.com>
2023-05-26 14:21:59 +0200

    first commit

---
+++ demo/src/test/java/FactoriaF5/talleresSomosf5BackEnd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package FactoriaF5.talleresSomosf5BackEnd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
